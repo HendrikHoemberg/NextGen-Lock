d42d266af625012f4c9cae8d412aca42e13e588b
Hendrik Hoemberg <hendrik.hoemberg@web.de>
2025-11-27 10:25:40 +0100

    Update datetime defaults in setupTables.sql to use localtime

--- db/setupTables.sql
+++ db/setupTables.sql
@@ -4,14 +4,14 @@ CREATE TABLE user (
     user_id     INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name  TEXT NOT NULL,
     last_name   TEXT NOT NULL,
-    created_at  TEXT NOT NULL DEFAULT (datetime('now'))
+    created_at  TEXT NOT NULL DEFAULT (datetime('now', 'localtime'))
 );
 
 CREATE TABLE rfid_card (
     card_uid     TEXT PRIMARY KEY,
     user_id     INTEGER,
     authorized  INTEGER NOT NULL DEFAULT 0,  
-    added_on    TEXT NOT NULL DEFAULT (datetime('now')),
+    added_on    TEXT NOT NULL DEFAULT (datetime('now', 'localtime')),
     
     FOREIGN KEY (user_id) REFERENCES user(user_id)
         ON DELETE SET NULL ON UPDATE CASCADE
@@ -20,7 +20,7 @@ CREATE TABLE rfid_card (
 CREATE TABLE access_log (
     log_id         INTEGER PRIMARY KEY AUTOINCREMENT,
     card_uid       TEXT NOT NULL,
-    access_time    TEXT NOT NULL DEFAULT (datetime('now')),
+    access_time    TEXT NOT NULL DEFAULT (datetime('now', 'localtime')),
     access_granted INTEGER NOT NULL,          
     note           TEXT
 );
